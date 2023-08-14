07688ec644439ff71b4d0c527817249b11c9ea1f
Richard <35225988+RichardMarkRoss@users.noreply.github.com>
2023-08-14 12:20:17 +0200

    update python binarytree and Song

--- Challenge 1 Python/Song.py
+++ Challenge 1 Python/Song.py
@@ -1,40 +1,23 @@
-# Define a class to represent a song
 class Song:
     def __init__(self, name):
         self.name = name
-        self.next = None  # Initialize the next song as None
+        self.next = None
 
-    # Method to set the next song in the playlist
     def next_song(self, song):
         self.next = song
 
-    # Method to check if the playlist is repeating
     def is_repeating_playlist(self):
-        # Initialize two pointers, slow and fast, both starting from the first song
-        slow = self
-        fast = self.next  # Assuming there's at least one song in the playlist
-
-        # Traverse the playlist
-        while fast is not None and fast.next is not None:
-            # If the slow and fast pointers meet, there's a repeating cycle
-            if slow == fast:
+        seen_songs = set()
+        current_song = self
+        while current_song is not None:
+            if current_song in seen_songs:
                 return True
-
-            # Move the slow pointer by one song
-            slow = slow.next
-            # Move the fast pointer by two songs
-            fast = fast.next.next
-
-        # If the fast pointer reaches the end, there's no repeating cycle
+            seen_songs.add(current_song)
+            current_song = current_song.next
         return False
 
-# Test the code
 first = Song("Hello")
 second = Song("Eye of the tiger")
-
-# Link the songs together to form a playlist
 first.next_song(second)
 second.next_song(first)
-
-# Check if the playlist is repeating and print the result
-print(first.is_repeating_playlist())  # Output: True
+print(first.is_repeating_playlist())
